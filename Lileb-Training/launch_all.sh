6d45d7e53767b333e6f3728986e43078ae6c2c29
Marc'Aurelio Ranzato <ranzato@devfair054.maas>
2019-10-14 12:13:26 -0700

    temporarily adding shell scripts

---
+++ Lileb-Training/launch_all.sh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+
+#source activate torch;
+
+RAY_MEM="$((300*1000*1000*1000))"
+echo "Assigning $RAY_MEM bytes for this node"
+
+if [ $SLURMD_NODENAME = $HEAD_NAME ]
+then
+    echo "I am head node: ${SLURMD_NODENAME}"
+    echo "Starting head node ..."
+    ray start --head --redis-port $HEAD_PORT --memory $RAY_MEM --temp-dir /tmp/ray_${USER}
+    echo "Done"
+
+    sleep 20
+    EXP_CONF_FILE=configs/${DOMAIN}/test_large.yaml
+    echo "Launching exp with config file: ${EXP_CONF_FILE}:"
+    cmd="python run.py with $EXP_CONF_FILE experiment.redis_address=localhost:$HEAD_PORT datasets.task_gen.samples_per_class=\"[${N_SAMPLES}, 200, 200]\""
+    echo ${cmd}
+    eval ${cmd}
+    echo "Job done, stopping head node."
+    ray stop
+else
+    sleep 10
+    echo "I am the worker ${SLURMD_NODENAME}, connecting to $HEAD_ADDR"
+    ray start --redis-address $HEAD_ADDR --memory $RAY_MEM --block --temp-dir /tmp/ray_veniat
+fi
---
+++ Lileb-Training/launch_worker.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+#source activate torch;
+export MASTER_ADDR="learnfair125"
+echo "Master is ${MASTER_ADDR}"
+
+echo "I am a worker: ${SLURMD_NODENAME}"
+echo "Connecting to ${MASTER_ADDR}"
+#python -c "import ray; ray.init(redis_address='$MASTER_ADDR:6385'); print(ray.available_resources())"
+ray start --redis-address $MASTER_ADDR:6385 --block
+
+#echo $SLURMD_NODENAME $SLURM_JOB_ID $CUDA_VISIBLE_DEVICES $SLURM_LOCALID
+#echo $SLURM_PROCID
+#echo $SLURM_NODELIST
+#hostname
+echo "okok ?"
